9b943301b0d53c4cd9653df9540f5ebef7452a49
yongheuicho <yongheuicho@gmail.com>
2021-06-09 05:50:44 +0900

    #14

--- pages/dogCatStat.vue
+++ pages/dogCatStat.vue
@@ -1,24 +1,9 @@
 <template>
-  <section class="section">
-    <article class="hero is-info">
-      <div class="hero-body">
-        <h1 class="title">애견 통계</h1>
-      </div>
-    </article>
-    <article class="message">
-      <div class="message-header">
-        애견 산업 전망
-      </div>
-      <div class="message-body">
-        adfssdfsd
-        <div class="content"></div>
-      </div>
-    </article>
-  </section>
+  <div></div>
 </template>
 
 <script>
-import myBarChart from "~/plugins/myBarChart";
+//import myBarChart from "~/plugins/myBarChart";
 export default {
   data() {}
   //   mounted() {
