0e03b17c4a19315406381948d135396f12430fbf
yongheuicho <yongheuicho@gmail.com>
2021-06-09 05:19:25 +0900

    #12

--- plugins/myBarChart.js
+++ plugins/myBarChart.js
@@ -2,6 +2,10 @@ export default function(elementName) {
   alert(elementName);
   var container = document.getElementById(elementName);
   alert(container);
+  if (container == null) {
+    alert("We are in null");
+    return;
+  }
   var data = {
     categories: ["2012", "2014", "2016", "2018", "2018"],
     series: [
