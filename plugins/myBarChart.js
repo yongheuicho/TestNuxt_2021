e677cc03835d17c2741a8ded57378b5b805808bf
yongheuicho <yongheuicho@gmail.com>
2021-06-09 05:22:47 +0900

    #13

--- plugins/myBarChart.js
+++ plugins/myBarChart.js
@@ -1,19 +1,19 @@
 export default function(elementName) {
-  alert(elementName);
-  var container = document.getElementById(elementName);
-  alert(container);
-  if (container == null) {
-    alert("We are in null");
-    return;
-  }
-  var data = {
-    categories: ["2012", "2014", "2016", "2018", "2018"],
-    series: [
-      {
-        name: "시장 규모 (억원)",
-        data: [9000, 14000, 23000, 37000, 58000]
-      }
-    ]
-  };
+  // alert(elementName);
+  // var container = document.getElementById(elementName);
+  // alert(container);
+  // if (container == null) {
+  //   alert("We are in null");
+  //   return;
+  // }
+  // var data = {
+  //   categories: ["2012", "2014", "2016", "2018", "2018"],
+  //   series: [
+  //     {
+  //       name: "시장 규모 (억원)",
+  //       data: [9000, 14000, 23000, 37000, 58000]
+  //     }
+  //   ]
+  // };
   //tui.chart.barChart(container, data);
 }
--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "#12"
+git commit -m "#13"
 git push -u origin master
