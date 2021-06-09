cbdacb2d5b9326ce8b2a68de81181933cde33558
yongheuicho <yongheuicho@gmail.com>
2021-06-09 12:22:54 +0900

    #19

--- pages/dogCatStat.vue
+++ pages/dogCatStat.vue
@@ -1,5 +1,18 @@
 <template>
-	<div>Test</div>
+	<div>
+		<section class="hero is-link">
+			<div class="hero-body">
+				<p class="title">
+					<i class="fas fa-dog"></i> 강아지와
+					<i class="fas fa-cat"></i> 고양이의 다양한 이름
+				</p>
+				<p class="subtile">
+					여러분의 강아지와 고양이에게 색다른 이름을 붙여주세요.
+				</p>
+			</div>
+		</section>
+		<hr />
+	</div>
 </template>
 <script>
 	import myBarChart from '~/plugins/myBarChart';
