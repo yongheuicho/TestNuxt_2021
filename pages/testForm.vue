304a000148974b0e0ca4447466f5158f1db1502c
yongheuicho <yongheuicho@gmail.com>
2021-06-11 17:41:12 +0900

    #35

--- pages/testForm.vue
+++ pages/testForm.vue
@@ -1,17 +1,3 @@
 <template>
 	<div>test1</div>
 </template>
-<script>
-	export default {
-		// data() {
-		// 	return {
-		// 		name: null,
-		// 	};
-		// },
-		// methods: {
-		// 	submitInput() {
-		// 		//				if (process.server) this.$router.push('/search?name=' + this.name);
-		// 	},
-		// },
-	};
-</script>
--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "#34"
+git commit -m "#35"
 git push -u origin master
